7f7d03e3da9e29605532f28acf87df29f8e57102
shoestealerz <michaelhanwchs@gmail.com>
2018-10-06 01:51:02 -0400

    Updates

---
+++ precache-manifest.1a867cd19bb976af9da7cea249c1e189.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce48ec2c77ef0201ec6c859ed37458e0",
+    "url": "/mywebsite/static/media/shoppinglist.ce48ec2c.gif"
+  },
+  {
+    "revision": "7a97346991c547779984",
+    "url": "/mywebsite/static/js/main.7a973469.chunk.js"
+  },
+  {
+    "revision": "acc54b3174e7cd2ff196",
+    "url": "/mywebsite/static/js/runtime~main.acc54b31.js"
+  },
+  {
+    "revision": "82352ec072dd6f1996c938cc844d5fc5",
+    "url": "/mywebsite/static/media/tenor.82352ec0.gif"
+  },
+  {
+    "revision": "3414d472c1784ac5d7ce769ed2647dce",
+    "url": "/mywebsite/static/media/asteroidsgame.3414d472.gif"
+  },
+  {
+    "revision": "d2f8d9fcba1ded8a658a04e83e9663f0",
+    "url": "/mywebsite/static/media/mymusiclyrics.d2f8d9fc.gif"
+  },
+  {
+    "revision": "3efb46f12013eb3ddeaaa88897dd027c",
+    "url": "/mywebsite/static/media/doubleliftheader.3efb46f1.png"
+  },
+  {
+    "revision": "a2c4508f63beae85b144702099dfecaa",
+    "url": "/mywebsite/static/media/doublecross.a2c4508f.gif"
+  },
+  {
+    "revision": "6ddc09c12f6efb95bed030da057df9ae",
+    "url": "/mywebsite/static/media/doubleliftface.6ddc09c1.png"
+  },
+  {
+    "revision": "2776fc9109779ca2e8e27759b52d20d8",
+    "url": "/mywebsite/static/media/backgroundlift.2776fc91.png"
+  },
+  {
+    "revision": "8eb27e991d3e2c19af79",
+    "url": "/mywebsite/static/js/1.8eb27e99.chunk.js"
+  },
+  {
+    "revision": "03ebd226a25db2d0a0673b3831881907",
+    "url": "/mywebsite/index.html"
+  }
+];
